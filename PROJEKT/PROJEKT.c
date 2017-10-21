bcd20faa77d97a61cc4a78458df5d9bd341e433e
svasso <samuel.vasso@gmail.com>
2017-10-21 20:09:59 +0200

    IZP zaciatok

--- PROJEKT/PROJEKT.c
+++ PROJEKT/PROJEKT.c
@@ -1,4 +1,16 @@
-int main() {
-    return 0;
+#include <stdio.h>
+#include <string.h>
+
+void pokus(/* arguments */) {
+  char c;
+  c=getchar();
+  printf("cislo je %c\n",&c );
 }
 
+
+int main() {
+
+s
+
+    return 0;
+}
