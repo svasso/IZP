9133a48199d873146cb1fd5301dc8cbc4e355bcd
svasso <samuel.vasso@gmail.com>
2017-10-23 15:35:18 +0200

    homeworks add

---
+++ homeworks/lecture4/tasks/exercise1.c
@@ -0,0 +1,10 @@
+/*
+    Implementujte funkci print_ascii, ktera dostane parametrem promennou typu char a vytiskne jeho ASCII hodnotu.
+    Napr. pro znak 'a' funkce vytiskne 97. 
+    Je povoleno pouzit funkci printf z knihovny stdio.h.
+    Ostatni knihovni funkce jsou zakazane!
+*/
+
+int main() {
+    return 0;
+}
