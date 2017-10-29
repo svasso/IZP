e1e1f4cafc5c77a8a1e35f93184e21ca885b2520
svasso <samuel.vasso@gmail.com>
2017-10-29 20:10:15 +0100

    progress

--- PROJEKT/PROJEKT.c
+++ PROJEKT/PROJEKT.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
+
+void StrDiff(char word1[42],char word2[42]) {
+for ( i = 0; i < 42; i++) {
+  if (strcmp(word1,word2)==0)
+    {
+    continue;
+
+    }
+      else break;
+  }
+
+}
+
+void AlphaDiff(char alpha1,char alpha2) {
+  if (getchar(alpha1)==getchar(alpha2)) {
+    printf("%c\n",&alpha1 );
+  }
+}
+
 int main(int argc,char *argv[]) {
 
 char arr[42][101];
 
 int i = 0;
 int j = 0;
-while ((scanf("%s", arr[i]) != EOF)&&(i < 40)) {
+while ((scanf("%s", arr[i]) != EOF)&&(i < 42)) {
   printf("%s\n", arr[i]);
   i++; //do 40
-
-  while (j<101) {
-	switch:
-	  j++;
-  }
-
-
 
 }
 
@@ -24,4 +36,3 @@ while ((scanf("%s", arr[i]) != EOF)&&(i < 40)) {
 
 return 0;
 }
-    
